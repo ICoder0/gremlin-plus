cbbffb844d498e15297a7ac0c43ba23ac72642ba
bofa1ex <bofa1exx@gmail.com>
2020-12-14 01:49:23 +0800

    Update: 支持evaluate expression调试.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/LambdaSupport.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/LambdaSupport.java
@@ -1,11 +1,11 @@
 package com.icoder0.gremlinplus.process.traversal.toolkit;
 
 import com.icoder0.gremlinplus.process.traversal.function.SerializedFunction;
-import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 
-import java.lang.invoke.SerializedLambda;
+import java.lang.invoke.*;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,6 +20,9 @@ public class LambdaSupport {
 
     /**
      * SerializedLambda 反序列化缓存
+     *
+     * key: User::getName
+     * value: serializedLambda(类名, 方法名, 字段名)
      */
     private static final Map<Class<?>, WeakReference<SerializedLambda>> FUNC_CACHE = new ConcurrentHashMap<>();
 
@@ -33,7 +36,28 @@ public class LambdaSupport {
      * @see #resolve(SerializedFunction)
      */
     public static <T> SerializedLambda resolve(SerializedFunction<T, ?> func) {
-        final Class<?> functionClazz = func.getClass();
+        Class<?> functionClazz = func.getClass();
+        // 支持evaluate expression调试, 走proxy代理, 每次缓存key都不命中, 因此不考虑缓存.
+        if (func instanceof Proxy) {
+            final Field field = FieldUtils.getDeclaredField(functionClazz.getSuperclass(), "h", true);
+            try {
+                final Object resp0 = field.get(func);
+                Field field2 = FieldUtils.getDeclaredField(resp0.getClass(), "val$target", true);
+                Object resp1 = field2.get(resp0);
+                Field field3 = FieldUtils.getDeclaredField(resp1.getClass(), "member", true);
+                Object resp2 = field3.get(resp1);
+                Field field4 = FieldUtils.getDeclaredField(resp2.getClass(), "name", true);
+                String methodName = (String) field4.get(resp2);
+                Field field5 = FieldUtils.getDeclaredField(resp2.getClass(), "clazz", true);
+                Class<?> clazz = (Class<?>) field5.get(resp2);
+                return new SerializedLambda(null, null, null,
+                        null, 0, clazz.getName(), methodName,
+                        null, null, new Object[0]
+                );
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return Optional.ofNullable(FUNC_CACHE.get(functionClazz))
                 .map(WeakReference::get)
                 .orElseGet(unchecked(() -> {

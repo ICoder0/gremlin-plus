c9c1aadcd77e8eb8b89df8330a21a3fdbb47ec26
bofa1ex <bofa1exx@gmail.com>
2020-12-15 14:19:32 +0800

    Chore: not effect change.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/VertexDefinitionSupport.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/VertexDefinitionSupport.java
@@ -1,15 +1,11 @@
 package com.icoder0.gremlinplus.process.traversal.toolkit;
 
-import com.icoder0.gremlinplus.annotation.GraphLabel;
-import com.icoder0.gremlinplus.annotation.VertexId;
-import com.icoder0.gremlinplus.annotation.VertexProperty;
 import com.icoder0.gremlinplus.process.traversal.definition.VertexDefinition;
 import com.icoder0.gremlinplus.process.traversal.definition.VertexPropertyDefinition;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -20,39 +16,25 @@ public class VertexDefinitionSupport {
 
     public static final Map<Class<?>, VertexDefinition> VERTEX_DEFINITION_MAP = new ConcurrentHashMap<>();
 
-    public static Optional<String> resolveLabel(Class<?> clazz) {
-        return Optional.ofNullable(clazz.getAnnotation(GraphLabel.class))
-                .map(GraphLabel::value);
-    }
-
     public static Map<String, VertexPropertyDefinition> resolveProperties(Class<?> clazz) {
         final Field[] fields = clazz.getDeclaredFields();
         Field primaryKeyField = null;
         final Map<String, VertexPropertyDefinition> vertexPropertyDefinitionMap = new HashMap<>();
         for (Field field : fields) {
-            if (field.getAnnotation(VertexId.class) != null) {
+            if (AnnotationSupport.checkVertexId(field)) {
                 if (primaryKeyField != null) {
-                    throw new IllegalArgumentException("VertexId不可以声明多个字段");
+                    throw ExceptionUtils.gpe(new IllegalArgumentException(String.format("{%s} @VertexId不可以声明多个字段", clazz.getName())));
                 }
                 primaryKeyField = field;
                 vertexPropertyDefinitionMap.put(primaryKeyField.getName(), VertexPropertyDefinition.builder()
                         .withPrimaryKey(true)
-                        .build());
-                continue;
+                        .build()
+                );
             }
-            final VertexPropertyDefinition vertexPropertyDefinition = Optional.ofNullable(field.getAnnotation(VertexProperty.class)).map(vertexProperty -> VertexPropertyDefinition.builder()
-                    .withPropertyName(vertexProperty.value())
-                    .withSerializable(vertexProperty.serializable())
-                    .build()
-            ).orElse(VertexPropertyDefinition.builder()
-                    .withPropertyName(field.getName())
-                    .withSerializable(true)
-                    .build()
-            );
-            vertexPropertyDefinitionMap.put(field.getName(), vertexPropertyDefinition);
+            vertexPropertyDefinitionMap.put(field.getName(), AnnotationSupport.resolveVertexProperty(field));
         }
         if (primaryKeyField == null){
-            throw new IllegalArgumentException("VertexId必须声明字段");
+            throw ExceptionUtils.gpe(new IllegalArgumentException(String.format("{%s} VertexId必须声明字段", clazz.getName())));
         }
         return vertexPropertyDefinitionMap;
     }

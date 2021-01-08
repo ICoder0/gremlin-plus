501328141fa7b1f15cd69eebc0bd445907751b89
bofa1ex <bofa1exx@gmail.com>
2021-01-08 15:33:19 +0800

    Update: 生成key扩展接口插件方式引入.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/KeyGeneratorSupport.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/KeyGeneratorSupport.java
@@ -3,17 +3,35 @@ package com.icoder0.gremlinplus.process.traversal.toolkit;
 import com.icoder0.gremlinplus.process.extension.DefaultKeyGenerator;
 import com.icoder0.gremlinplus.process.extension.KeyGenerator;
 
+import java.util.Optional;
+
 /**
  * @author bofa1ex
  * @since 2020/12/23
  */
 public class KeyGeneratorSupport {
-    protected static KeyGenerator keyGenerator = DefaultKeyGenerator.getInstance();
 
-    public static Object generate(){
-        if (keyGenerator == null){
-            throw new IllegalArgumentException("keyGenerator不可为空");
+    private static Class<? extends KeyGenerator> keyGeneratorPlugin;
+
+    public static class KeyGeneratorHolder {
+        public static KeyGenerator keyGenerator;
+
+        static {
+            keyGenerator = Optional.ofNullable(keyGeneratorPlugin).map(CglibSupport::newInstance)
+                    .map(o -> (KeyGenerator) o)
+                    .orElseGet(DefaultKeyGenerator::getInstance);
         }
-        return keyGenerator.key();
+    }
+
+    public static void init(Class<? extends KeyGenerator> plugin) {
+        keyGeneratorPlugin = plugin;
+    }
+
+    public static <T extends KeyGenerator> void init(T t) {
+        KeyGeneratorHolder.keyGenerator = t;
+    }
+
+    public static Object generate() {
+        return KeyGeneratorHolder.keyGenerator.key();
     }
 }

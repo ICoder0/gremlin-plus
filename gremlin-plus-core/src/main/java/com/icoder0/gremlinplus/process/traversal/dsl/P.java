ad6decb886a7cfaddcac0721ae0baa3e2b9a6f41
bofa1ex <bofa1exx@gmail.com>
2021-01-08 15:34:05 +0800

    Add: 新增notnull/isnull/empty断言方法.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/P.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/P.java
@@ -1,5 +1,7 @@
 package com.icoder0.gremlinplus.process.traversal.dsl;
 
+import org.apache.tinkerpop.gremlin.process.traversal.Compare;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.BiPredicate;
@@ -15,6 +17,13 @@ public class P<V> extends org.apache.tinkerpop.gremlin.process.traversal.P<V> {
     }
 
     /**
+     * Determines a empty predicate when property is not support serializable.
+     */
+    public static <V> org.apache.tinkerpop.gremlin.process.traversal.P<V> empty() {
+        return new P<>((ignore0,ignore1) -> true, null);
+    }
+
+    /**
      * Determines if a value is within the specified list of values.
      *
      * @since 3.0.0-incubating
@@ -26,9 +35,18 @@ public class P<V> extends org.apache.tinkerpop.gremlin.process.traversal.P<V> {
     /**
      * Determines if a value is not within the specified list of values.
      *
+     *
      * @since 3.0.0-incubating
      */
     public static <V> org.apache.tinkerpop.gremlin.process.traversal.P<V> flatWithin(final V... values) {
         return flatWithin(Arrays.asList(values));
+    }
+
+    public static <V> org.apache.tinkerpop.gremlin.process.traversal.P<V> notnull(){
+        return new P(Compare.neq, null);
+    }
+
+    public static <V> org.apache.tinkerpop.gremlin.process.traversal.P<V> isnull(){
+        return new P(Compare.eq, null);
     }
 }

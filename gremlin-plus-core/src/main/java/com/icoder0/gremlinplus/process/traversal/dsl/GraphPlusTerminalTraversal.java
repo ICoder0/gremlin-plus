67a79d6562ada6dd50a4fb9bb9aa868e6fe35917
bofa1ex <bofa1exx@gmail.com>
2021-01-08 15:47:09 +0800

    Add: 新增tryToBean/toPair/toBean/toPairList/toBeanList/toPairSet/toBeanSet方法返回值支持optional. Update: 更新方法逻辑(toBeanSet/toPairSet/toBeanList/toPairList/toBean/toPair)引入vertex元数据缓存扩展入口. Chore: 引入日志依赖, tryToBean异常的日志输出.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/GraphPlusTerminalTraversal.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/GraphPlusTerminalTraversal.java
@@ -2,19 +2,22 @@ package com.icoder0.gremlinplus.process.traversal.dsl;
 
 import com.icoder0.gremlinplus.process.traversal.definition.VertexDefinition;
 import com.icoder0.gremlinplus.process.traversal.definition.VertexPropertyDefinition;
-import com.icoder0.gremlinplus.process.traversal.toolkit.CglibSupport;
-import com.icoder0.gremlinplus.process.traversal.toolkit.ExceptionUtils;
+import com.icoder0.gremlinplus.process.traversal.toolkit.*;
 import net.sf.cglib.beans.BeanMap;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.tinkerpop.gremlin.process.traversal.TraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 import java.util.stream.Stream;
 
-import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.VERTEX_DEFINITION_MAP;
-import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.VERTEX_UNSERIALIZED_MAP;
+import static com.icoder0.gremlinplus.process.traversal.toolkit.UnSerializedPropertySupport.getUnSerializedPropertyCache;
+import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.getVertexDefinitionCache;
+import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.resolveProperties;
+
 
 /**
  * @author bofa1ex
@@ -22,8 +25,9 @@ import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinition
  */
 public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversal<S, E, L> {
 
-    public GraphPlusTerminalTraversal(final GraphPlusNormalTraversal<S, E, L> normalTraversal) {
-        super((TraversalSource) normalTraversal);
+    private static final Logger log = LoggerFactory.getLogger(GraphPlusTerminalTraversal.class);
+
+    public GraphPlusTerminalTraversal() {
     }
 
     public GraphPlusTerminalTraversal(final TraversalSource traversalSource) {
@@ -47,9 +51,16 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
     public Optional<L> tryToBean() {
         try {
             return Optional.ofNullable(toBean());
-        } catch (ExceptionUtils.CheckedException e) {
+        } catch (Exception e) {
+            log.error("toBean#invoke异常", e);
             return Optional.empty();
         }
+    }
+
+
+    public Optional<L> tryToBean(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return tryToBean();
     }
 
     public Pair<L, Vertex> toPair() {
@@ -58,7 +69,14 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
         }
         final Vertex vertex = ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).tryNext().orElseThrow(() -> ExceptionUtils.gpe(String.format("找不到对应{%s}#vertex记录", labelEntityClass.getName())));
         final L o = (L) CglibSupport.newInstance(labelEntityClass);
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         for (Map.Entry<String, VertexPropertyDefinition> entry : vertexDefinition.getVertexPropertyDefinitionMap().entrySet()) {
             final VertexPropertyDefinition vertexPropertyDefinition = entry.getValue();
@@ -67,10 +85,15 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                 continue;
             }
             Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                    .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                    .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                     .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
         }
         return Pair.of(o, vertex);
+    }
+
+    public Pair<L, Vertex> toPair(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toPair();
     }
 
     public L toBean() {
@@ -80,7 +103,14 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
 
         final Vertex vertex = ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).tryNext().orElseThrow(() -> ExceptionUtils.gpe(String.format("找不到对应{%s}#vertex记录", labelEntityClass.getName())));
         final L o = (L) CglibSupport.newInstance(labelEntityClass);
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         for (Map.Entry<String, VertexPropertyDefinition> entry : vertexDefinition.getVertexPropertyDefinitionMap().entrySet()) {
             final VertexPropertyDefinition vertexPropertyDefinition = entry.getValue();
@@ -89,17 +119,30 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                 continue;
             }
             Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                    .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                    .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                     .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
         }
         return o;
+    }
+
+
+    public L toBean(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toBean();
     }
 
     public List<Pair<L, Vertex>> toPairList() {
         if (Objects.isNull(labelEntityClass)) {
             throw ExceptionUtils.gpe("必须有声明labelEntity的step");
         }
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         final List<Pair<L, Vertex>> pairs = new ArrayList<>();
         for (Vertex vertex : ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).toList()) {
@@ -111,7 +154,7 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                     continue;
                 }
                 Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                         .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
             }
             pairs.add(Pair.of(o, vertex));
@@ -119,11 +162,25 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
         return pairs;
     }
 
+
+    public List<Pair<L, Vertex>> toPairList(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toPairList();
+    }
+
+
     public List<L> toBeanList() {
         if (Objects.isNull(labelEntityClass)) {
             throw ExceptionUtils.gpe("必须有声明labelEntity的step");
         }
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         final List<L> beans = new ArrayList<>();
         for (Vertex vertex : ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).toList()) {
@@ -135,7 +192,7 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                     continue;
                 }
                 Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                         .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
             }
             beans.add(o);
@@ -143,11 +200,24 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
         return beans;
     }
 
+
+    public List<L> toBeanList(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toBeanList();
+    }
+
     public Set<Pair<L, Vertex>> toPairSet() {
         if (Objects.isNull(labelEntityClass)) {
             throw ExceptionUtils.gpe("必须有声明labelEntity的step");
         }
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         final Set<Pair<L, Vertex>> pairs = new HashSet<>();
         for (Vertex vertex : ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).toList()) {
@@ -159,7 +229,7 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                     continue;
                 }
                 Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                         .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
             }
             pairs.add(Pair.of(o, vertex));
@@ -167,11 +237,24 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
         return pairs;
     }
 
+
+    public Set<Pair<L, Vertex>> toPairSet(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toPairSet();
+    }
+
     public Set<L> toBeanSet() {
         if (Objects.isNull(labelEntityClass)) {
             throw ExceptionUtils.gpe("必须有声明labelEntity的step");
         }
-        final VertexDefinition vertexDefinition = VERTEX_DEFINITION_MAP.get(labelEntityClass);
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(labelEntityClass);
+        final VertexDefinition vertexDefinition = getVertexDefinitionCache().putIfAbsent(labelEntityClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(labelEntityClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(labelEntityClass))
+                .withBeanMap(generator.create())
+                .build()
+        );
         final BeanMap beanMap = vertexDefinition.getBeanMap();
         final Set<L> beans = new HashSet<>();
         for (Vertex vertex : ((GraphPlusNormalTraversal<S, Vertex, L>) this.asAdmin()).toSet()) {
@@ -183,7 +266,7 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
                     continue;
                 }
                 Optional.ofNullable(vertex.property(vertexPropertyDefinition.getPropertyName()).orElse(null))
-                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : VERTEX_UNSERIALIZED_MAP.getOrDefault(value, null))
+                        .map(value -> vertexPropertyDefinition.isSerializable() ? value : getUnSerializedPropertyCache().get(value))
                         .ifPresent(value -> beanMap.put(o, entry.getKey(), value));
             }
             beans.add(o);
@@ -191,11 +274,26 @@ public class GraphPlusTerminalTraversal<S, E, L> extends GraphPlusNormalTraversa
         return beans;
     }
 
+
+    public Set<L> toBeanSet(Class<L> clazz) {
+        this.labelEntityClass = clazz;
+        return toBeanSet();
+    }
+
+
     public Stream<Pair<L, Vertex>> toPairStream() {
         return toPairSet().stream();
     }
 
     public Stream<L> toBeanStream() {
         return toBeanSet().stream();
+    }
+
+    public Stream<Pair<L, Vertex>> toPairStream(Class<L> clazz) {
+        return toPairSet(clazz).stream();
+    }
+
+    public Stream<L> toBeanStream(Class<L> clazz) {
+        return toBeanSet(clazz).stream();
     }
 }

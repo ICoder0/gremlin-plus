c3d6efe29a6ebfbafedca13cbdf2262da426dba1
bofa1ex <bofa1exx@gmail.com>
2021-01-08 15:19:29 +0800

    Update: 引入vertex元数据缓存入口(元数据录入), 移除冗余方法.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/SerializedFunctionSupport.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/toolkit/SerializedFunctionSupport.java
@@ -4,40 +4,24 @@ import com.icoder0.gremlinplus.process.traversal.definition.VertexDefinition;
 import com.icoder0.gremlinplus.process.traversal.definition.VertexPropertyDefinition;
 import com.icoder0.gremlinplus.process.traversal.function.SerializedFunction;
 import com.icoder0.gremlinplus.process.traversal.function.ThrowingSupplier;
+import net.sf.cglib.beans.BeanMap;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.lang.invoke.SerializedLambda;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
-import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.VERTEX_DEFINITION_MAP;
+import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.getVertexDefinitionCache;
+import static com.icoder0.gremlinplus.process.traversal.toolkit.VertexDefinitionSupport.resolveProperties;
+
 
 /**
  * @author bofa1ex
  * @since 2020/12/15
  */
 public class SerializedFunctionSupport {
-
-    /**
-     * 根据lambda getter/setter表达式获取对应类的字段
-     * 再根据对应类的字段获取其在实体类中声明的属性名.
-     * 如果该字段声明不可持久化, 会由{@link KeyGeneratorSupport#generate()}生成唯一id作为持久化内容.
-     * 并将该字段值存入进程内存.
-     *
-     * @return tuple with property name and key(which field is not support serialize).
-     */
-    public static Pair<String, Object> method2PropertyKeyPair(SerializedLambda lambda) {
-        final Class<?> implClass = ThrowingSupplier.unchecked(() -> LambdaSupport.getImplClass(lambda)).get();
-        final String fieldName = PropertyNamerSupport.resolvePropertyName(lambda.getImplMethodName());
-        return Optional.ofNullable(VERTEX_DEFINITION_MAP.get(implClass))
-                .map(VertexDefinition::getVertexPropertyDefinitionMap)
-                .map(vertexPropertyDefinitionMap -> vertexPropertyDefinitionMap.get(fieldName))
-                .map(vertexPropertyDefinition -> vertexPropertyDefinition.isSerializable() ?
-                        ImmutablePair.of(vertexPropertyDefinition.getPropertyName(), null) :
-                        ImmutablePair.of(vertexPropertyDefinition.getPropertyName(), KeyGeneratorSupport.generate()))
-                .orElse(ImmutablePair.nullPair());
-    }
 
     /**
      * 根据lambda getter/setter表达式获取对应类的字段
@@ -49,7 +33,12 @@ public class SerializedFunctionSupport {
     public static Pair<String, Boolean> method2PropertyBoolPair(SerializedLambda lambda) {
         final Class<?> implClass = ThrowingSupplier.unchecked(() -> LambdaSupport.getImplClass(lambda)).get();
         final String fieldName = PropertyNamerSupport.resolvePropertyName(lambda.getImplMethodName());
-        return Optional.ofNullable(VERTEX_DEFINITION_MAP.get(implClass))
+        return Optional.of(VertexDefinitionSupport.getVertexDefinitionCache().putIfAbsent(implClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(implClass))
+                .withVertexPropertyDefinitionMap(VertexDefinitionSupport.resolveProperties(implClass))
+                .withBeanMap(BeanMap.create(CglibSupport.newInstance(implClass)))
+                .build()))
+
                 .map(VertexDefinition::getVertexPropertyDefinitionMap)
                 .map(vertexPropertyDefinitionMap -> vertexPropertyDefinitionMap.get(fieldName))
                 .map(vertexPropertyDefinition -> ImmutablePair.of(vertexPropertyDefinition.getPropertyName(), vertexPropertyDefinition.isSerializable()))
@@ -60,7 +49,14 @@ public class SerializedFunctionSupport {
     public static String method2Property(SerializedLambda lambda) {
         final Class<?> implClass = ThrowingSupplier.unchecked(() -> LambdaSupport.getImplClass(lambda)).get();
         final String fieldName = PropertyNamerSupport.resolvePropertyName(lambda.getImplMethodName());
-        return Optional.ofNullable(VERTEX_DEFINITION_MAP.get(implClass))
+        final BeanMap.Generator generator = new BeanMap.Generator();
+        generator.setBeanClass(implClass);
+        return Optional.ofNullable(getVertexDefinitionCache().putIfAbsent(implClass, () -> VertexDefinition.builder()
+                .withLabel(AnnotationSupport.resolveVertexLabel(implClass))
+                .withVertexPropertyDefinitionMap(resolveProperties(implClass))
+                .withBeanMap(generator.create())
+                .build()))
+
                 .map(VertexDefinition::getVertexPropertyDefinitionMap)
                 .map(vertexPropertyDefinitionMap -> vertexPropertyDefinitionMap.get(fieldName))
                 .map(VertexPropertyDefinition::getPropertyName)

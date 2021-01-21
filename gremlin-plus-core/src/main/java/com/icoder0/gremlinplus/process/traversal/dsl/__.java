70feaeb64ec6c09405f02d7761fddb87a41b1f7f
bofa1ex <bofa1exx@gmail.com>
2021-01-21 11:35:05 +0800

    Add: 新增匿名Traversal类addV方法入口.

--- gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/__.java
+++ gremlin-plus-core/src/main/java/com/icoder0/gremlinplus/process/traversal/dsl/__.java
@@ -2,6 +2,7 @@ package com.icoder0.gremlinplus.process.traversal.dsl;
 
 
 import com.icoder0.gremlinplus.process.traversal.function.SerializedFunction;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
 /**
  * @author bofa1ex
@@ -11,6 +12,10 @@ public class __ {
 
     private static <S, E, L> GraphPlusTerminalTraversal<S, E, L> start() {
         return new GraphPlusTerminalTraversal<>();
+    }
+
+    public static <S, E, L> GraphPlusTerminalTraversal<Vertex, Vertex, L> addV(final Class<L> clazz) {
+        return __.<S, E, L>start().addV(clazz);
     }
 
     public static <S, E, L> GraphPlusTerminalTraversal<S, E, L> hasLabel(final Class<L> clazz) {
